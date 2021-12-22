4643153bd9aecdda091a97ee7c7447b9928e35cf
mantrishiva <shivaprasadm613@gmail.com>
2021-12-22 11:00:26 +0530

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoitem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1'},
+    { text: 'create an app', key: '2'},
+    { text: 'play on the switch', key: '3'}
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevtodo) => {
+      return prevtodo.filter(item => item.key != key)
+    })
+  }
+
+  const submitHandler = (text) => {
+    if(text.length > 0){
+    setTodos((prevtodo) => {
+      return [{text: text, key: Math.random().toString()},...prevtodo]
+    })}else{
+      Alert.alert('oops!', 'Type any text', [
+        {text: 'Understood', onPress: () => console.log('alert closed')}
+      ])
+    }
+  }
+
   return (
+    <TouchableWithoutFeedback onPress={()=> {
+      Keyboard.dismiss();
+      console.log('dismissed keyboard')
+    }}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.content}>
+        <AddTodo submitHandler={submitHandler} />
+        <View style={styles.list}>
+          <FlatList 
+            data={todos}
+            renderItem={({item}) => (
+              <TodoItem item={item} pressHandler={pressHandler}/>
+            )}
+          />
+        </View>
+      </View>
     </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -15,7 +56,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  content: {
+    padding: 40,
+    flex: 1
+  },
+  list: {
+    flex:1,
+    marginTop: 10,
+  }
+  
 });
